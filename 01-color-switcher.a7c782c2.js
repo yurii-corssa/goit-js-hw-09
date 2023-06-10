12ccad862b145a25d2124c665737e64d80b1d869
yurii-corssa <122684054+yurii-corssa@users.noreply.github.com>
2023-06-10 19:45:11 +0000

    Deploying to gh-pages from @ yurii-corssa/goit-js-hw-09@987f6883ba420eb7d81d72f0a3f4c0c7e11606e7 🚀

---
+++ 01-color-switcher.a7c782c2.js
@@ -0,0 +1,2 @@
+!function(){var t={startBtn:document.querySelector("[data-start]"),stopBtn:document.querySelector("[data-stop]"),body:document.querySelector("body")};t.startBtn.addEventListener("click",(function(){timerId=setInterval((function(){t.body.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16).padStart(6,0))}),1e3),t.startBtn.disabled=!0,t.stopBtn.disabled=!1})),t.stopBtn.addEventListener("click",(function(){clearInterval(timerId),t.startBtn.disabled=!1,t.stopBtn.disabled=!0}))}();
+//# sourceMappingURL=01-color-switcher.a7c782c2.js.map
