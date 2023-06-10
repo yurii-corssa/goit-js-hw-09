987f6883ba420eb7d81d72f0a3f4c0c7e11606e7
yurii-corssa <yurii.corssa@gmail.com>
2023-06-10 21:44:43 +0200

    task 01

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,29 @@
+const refs = {
+  startBtn: document.querySelector('[data-start]'),
+  stopBtn: document.querySelector('[data-stop]'),
+  body: document.querySelector('body'),
+};
 
+refs.startBtn.addEventListener('click', startSwitchColors);
+refs.stopBtn.addEventListener('click', stopSwitchColor);
+
+function startSwitchColors() {
+  timerId = setInterval(() => {
+    refs.body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+
+  refs.startBtn.disabled = true;
+  refs.stopBtn.disabled = false;
+}
+
+function stopSwitchColor() {
+  clearInterval(timerId);
+  refs.startBtn.disabled = false;
+  refs.stopBtn.disabled = true;
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
