42167942786f04f8b3b297059288ec46ecbee71a
yurii-corssa <yurii.corssa@gmail.com>
2023-06-11 13:22:18 +0200

    task 03

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,49 @@
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
+const form = document.querySelector('.form');
+
+form.addEventListener('submit', event => {
+  event.preventDefault();
+
+  const { delay, step, amount } = event.currentTarget;
+  const delayValue = Number(delay.value);
+  const stepValue = Number(step.value);
+  const amountValue = Number(amount.value);
+
+  runQueue(delayValue, stepValue, amountValue);
+});
+
+function runQueue(delay, step, amount) {
+  setTimeout(() => {
+    let intervalNumber = 1;
+
+    createPromise(intervalNumber, delay);
+
+    const intervalId = setInterval(() => {
+      if (intervalNumber === amount) {
+        clearInterval(intervalId);
+        return;
+      }
+
+      intervalNumber += 1;
+
+      createPromise(intervalNumber, delay);
+    }, step);
+  }, delay);
+}
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+
+  const promise = new Promise((resolve, reject) => {
+    if (shouldResolve) {
+      resolve(`✅ Fulfilled promise ${position} in ${delay}ms`);
+    }
+
+    reject(`❌ Rejected promise ${position} in ${delay}ms`);
+  });
+
+  return promise
+    .then(result => Notify.success(result))
+    .catch(result => Notify.failure(result));
 }
